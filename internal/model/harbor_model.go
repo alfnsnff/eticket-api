c80f387bd2a79e7923267681e6547ff1f129d6a2
alfnsnff <alfnsnff@gmail.com>
2025-05-03 22:43:54 +0700

    fix: revise

--- internal/model/harbor_model.go
+++ internal/model/harbor_model.go
@@ -8,17 +8,23 @@ import (
 type ReadHarborResponse struct {
 	ID        uint      `json:"id"`
 	Name      string    `json:"name"`
+	Status    string    `json:"status"`
+	Year      string    `json:"year"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // HarborDTO represents a harbor.
 type WriteHarborRequest struct {
-	Name string `json:"name"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	Year   string `json:"year"`
 }
 
 // HarborDTO represents a harbor.
 type UpdateHarborRequest struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
+	ID     uint   `json:"id"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	Year   string `json:"year"`
 }
