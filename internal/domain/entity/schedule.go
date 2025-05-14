b973dee4d663d3f6092b15e7b12c24b29704a089
alfnsnff <alfnsnff@gmail.com>
2025-05-14 15:56:35 +0700

    hotfix: fix naming

--- internal/domain/entity/schedule.go
+++ internal/domain/entity/schedule.go
@@ -3,13 +3,14 @@ package entity
 import "time"
 
 type Schedule struct {
-	ID               uint      `gorm:"column:id;primaryKey"`
-	RouteID          uint      `gorm:"column:route_id;not null;index"`
-	ShipID           uint      `gorm:"column:ship_id;not null;index"`
-	ScheduleDatetime time.Time `gorm:"column:schedule_datetime;not null"`
-	Status           *string   `gorm:"column:status;type:varchar(24);not null"`
-	CreatedAt        time.Time `gorm:"column:created_at;not null"`
-	UpdatedAt        time.Time `gorm:"column:updated_at;not null"`
+	ID                uint       `gorm:"column:id;primaryKey"`
+	RouteID           uint       `gorm:"column:route_id;not null;index"`
+	ShipID            uint       `gorm:"column:ship_id;not null;index"`
+	DepartureDatetime *time.Time `gorm:"column:departure_datetime;"`
+	ArrivalDatetime   *time.Time `gorm:"column:arrival_datetime;"`
+	Status            *string    `gorm:"column:status;type:varchar(24);not null"`
+	CreatedAt         time.Time  `gorm:"column:created_at;not null"`
+	UpdatedAt         time.Time  `gorm:"column:updated_at;not null"`
 }
 
 func (sch *Schedule) TableName() string {
