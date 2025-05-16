38400cfa89562f9f358df445d8865e819fdb360c
alfnsnff <alfnsnff@gmail.com>
2025-05-16 20:25:46 +0700

    hotfix: set auth token to cookie

--- internal/delivery/http/middleware/auth_middleware.go
+++ internal/delivery/http/middleware/auth_middleware.go
@@ -4,7 +4,6 @@ import (
 	"eticket-api/pkg/utils/helper/auth"
 	"eticket-api/pkg/utils/helper/response"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,21 +11,27 @@ import (
 // Authenticate validates the JWT token and injects user info into context.
 func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Missing Authorization header", nil))
-			return
-		}
+		// authHeader := c.GetHeader("Authorization")
+		// if authHeader == "" {
+		// 	c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Missing Authorization header", nil))
+		// 	return
+		// }
 
-		// More efficient split for "Bearer <token>"
-		scheme, token, found := strings.Cut(authHeader, " ")
-		if !found || strings.ToLower(scheme) != "bearer" || token == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Authorization header must be in 'Bearer <token>' format", nil))
+		// // More efficient split for "Bearer <token>"
+		// scheme, token, found := strings.Cut(authHeader, " ")
+		// if !found || strings.ToLower(scheme) != "bearer" || token == "" {
+		// 	c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Authorization header must be in 'Bearer <token>' format", nil))
+		// 	return
+		// }
+
+		tokenStr, err := c.Cookie("access_token")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Missing token", err.Error()))
 			return
 		}
 
 		// Validate JWT token
-		claims, err := auth.ValidateToken(token)
+		claims, err := auth.ValidateToken(tokenStr)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, response.NewErrorResponse("Invalid or expired token", err.Error()))
 			return
@@ -35,7 +40,7 @@ func Authenticate() gin.HandlerFunc {
 		// Inject values into Gin context
 		c.Set("userID", claims.ID)
 		c.Set("username", claims.Username)
-		c.Set("token", token) // Optional: allow downstream logic access to raw token
+		c.Set("token", tokenStr) // Optional: allow downstream logic access to raw token
 
 		c.Next()
 	}
